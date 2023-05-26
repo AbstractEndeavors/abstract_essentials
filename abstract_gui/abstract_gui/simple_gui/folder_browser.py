4e1e9c38c35e454a4d5e414aa1bbde18b1bfdc97
putkoff <57512254+putkoff@users.noreply.github.com>
2023-05-26 18:52:08 -0500

    Update folder_browser.py

--- abstract_gui/abstract_gui/simple_gui/folder_browser.py
+++ abstract_gui/abstract_gui/simple_gui/folder_browser.py
@@ -1,5 +1,6 @@
-def browser():
-    window = sg.Window('directory_browser',[[sg.Input('',key="selection"),sg.FolderBrowse("Select File", key="-FOLDER_SELECT-", initial_folder="/home/bigrugz/Documents", target="-FOLDER_SELECT-", enable_events=True), sg.Button("select", key="-GRAB_FILE-", enable_events=True), sg.Button("EXIT", key="-EXIT-")]], resizable=True, finalize=True)
+import os
+def browser(initial_folder:str=os.getcwd()):
+    window = sg.Window('directory_browser',[[sg.Input('',key="selection"),sg.FolderBrowse("Select File", key="-FOLDER_SELECT-", initial_folder=initial_folder, target="-FOLDER_SELECT-", enable_events=True), sg.Button("select", key="-GRAB_FILE-", enable_events=True), sg.Button("EXIT", key="-EXIT-")]], resizable=True, finalize=True)
     while True:
         event,values = window.read()
         if event == '-GRAB_FILE-':
