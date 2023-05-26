de4f50b1f63b98ce03c22f57d2232ad562f64cfe
putkoff <57512254+putkoff@users.noreply.github.com>
2023-05-26 18:48:50 -0500

    Update message_aggregator.py

--- abstract_ai/abstract_ai/message_aggregator.py
+++ abstract_ai/abstract_ai/message_aggregator.py
@@ -56,14 +56,6 @@ def aggregate_conversations(directory):
                 print(lsAll)
     chat_log_file.close()
     return aggregated_conversations
-def browser():
-    window = sg.Window('directory_browser',[[sg.Input('',key="selection"),sg.FolderBrowse("Select File", key="-FOLDER_SELECT-", initial_folder="/home/bigrugz/Documents", target="-FOLDER_SELECT-", enable_events=True), sg.Button("select", key="-GRAB_FILE-", enable_events=True), sg.Button("EXIT", key="-EXIT-")]], resizable=True, finalize=True)
-    while True:
-        event,values = window.read()
-        if event == '-GRAB_FILE-':
-            window.close()
-            break
-    return values["selection"]
 def get_responses():
     if "response_data" not in os.listdir(os.getcwd()):
         aggregate_conversations(browser())
