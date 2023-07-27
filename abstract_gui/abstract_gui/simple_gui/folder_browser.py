c020bec930fc355abe5ac68fa0d765717eb52305
putkoff <57512254+putkoff@users.noreply.github.com>
2023-07-27 18:52:01 -0500

    Delete abstract_gui/abstract_gui directory

--- abstract_gui/abstract_gui/simple_gui/folder_browser.py
+++
@@ -1,9 +0,0 @@
-import os
-def browser(initial_folder:str=os.getcwd()):
-    window = sg.Window('directory_browser',[[sg.Input('',key="selection"),sg.FolderBrowse("Select File", key="-FOLDER_SELECT-", initial_folder=initial_folder, target="-FOLDER_SELECT-", enable_events=True), sg.Button("select", key="-GRAB_FILE-", enable_events=True), sg.Button("EXIT", key="-EXIT-")]], resizable=True, finalize=True)
-    while True:
-        event,values = window.read()
-        if event == '-GRAB_FILE-':
-            window.close()
-            break
-    return values["selection"]
