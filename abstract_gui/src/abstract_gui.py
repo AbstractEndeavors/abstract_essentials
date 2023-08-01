b9e5d60efc4bf757cab8ba0d87a2252f051b39a8
putkoff <57512254+putkoff@users.noreply.github.com>
2023-08-01 05:20:32 -0500

    Update abstract_gui.py

--- abstract_gui/src/abstract_gui.py
+++ abstract_gui/src/abstract_gui.py
@@ -3,49 +3,109 @@ from abstract_utilities.class_utils import get_fun
 from abstract_utilities.thread_utils import thread_alive
 from abstract_utilities.path_utils import get_current_path
 class WindowGlobalBridge:
+    """
+    A class to manage the global variables shared between different scripts.
+
+    Attributes:
+        global_vars (dict): A dictionary to store global variables for each script.
+
+    Methods:
+        __init__(self):
+            Initializes the WindowGlobalBridge with an empty dictionary for global_vars.
+
+        retrieve_global_variables(self, script_name, global_variables, tag_script_name=False):
+            Stores the global variables of a script in the global_vars dictionary.
+
+        return_global_variables(self, script_name=None):
+            Returns the global variables of a script.
+
+        change_globals(self, variable, value, script_name=None):
+            Modifies a global variable value for a specified script.
+
+        search_globals_values(self, value, script_name=None):
+            Searches for a specific value in the global variables of a script.
+
+        return_global_value(self, variable, script_name=None):
+            Returns the value of a specific global variable in a script.
+    """
+    def __init__(self):
         """
-        A class to manage the global variables shared between different scripts.
-
-        Attributes:
-                global_vars (dict): A dictionary to store global variables for each script.
-        
-        Methods:
-                __init__(self):
-                    Initializes the WindowGlobalBridge with an empty dictionary for global_vars.
-
-                retrieve_global_variables(self, script_name, global_variables):
-                    Stores the global variables of a script in the global_vars dictionary.
-
-                return_global_variables(self, script_name):
-                    Returns the global variables of a script.
+        Initializes the WindowGlobalBridge with an empty dictionary for global_vars.
         """
-        def __init__(self):
-                """
-                Initializes the WindowGlobalBridge with an empty dictionary for global_vars.
-                """
-                self.global_vars = {}
+        self.global_vars = {}
 
-        def retrieve_global_variables(self, script_name, global_variables):
-                """
-                Stores the global variables of a script in the global_vars dictionary.
+    def retrieve_global_variables(self, script_name:str, global_variables:dict, tag_script_name:bool=False):
+        """
+        Stores the global variables of a script in the global_vars dictionary.
 
-                Args:
-                    script_name (str): The name of the script.
-                    global_variables (dict): The global variables to store for the script.
-                """
-                self.global_vars[script_name] = global_variables
-        
-        def return_global_variables(self, script_name):
-                """
-                Returns the global variables of a script.
+        Args:
+            script_name (str): The name of the script.
+            global_variables (dict): The global variables to store for the script.
+            tag_script_name (bool, optional): If True, the script_name will be stored in the global_variables dictionary.
+                                              Defaults to False.
+        """
+        self.global_vars[script_name] = global_variables
+        if tag_script_name:
+            self.global_vars[script_name]["script_name"] = script_name
 
-                Args:
-                    script_name (str): The name of the script.
+    def return_global_variables(self, script_name=None):
+        """
+        Returns the global variables of a script.
 
-                Returns:
-                    dict: The global variables of the script. If no global variables are found, it returns an empty dictionary.
-                """
-                return self.global_vars.get(script_name, {})
+        Args:
+            script_name (str, optional): The name of the script. If None, all global variables will be returned.
+
+        Returns:
+            dict: The global variables of the script. If no global variables are found, it returns an empty dictionary.
+        """
+        if script_name is not None:
+            return self.global_vars.get(script_name, {})
+        else:
+            return self.global_vars
+
+    def change_globals(self, variable:str, value:any, script_name:str=None):
+        """
+        Modifies a global variable value for a specified script.
+
+        Args:
+            variable (str): The name of the global variable to modify.
+            value (any): The new value to assign to the global variable.
+            script_name (str, optional): The name of the script. If None, the global variable in the base context will be modified.
+        """
+        if script_name is not None:
+            self.global_vars[script_name][variable] = value
+
+    def search_globals_values(self, value:any, script_name:str=None):
+        """
+        Searches for a specific value in the global variables of a script.
+
+        Args:
+            value (any): The value to search for in the global variables.
+            script_name (str, optional): The name of the script. If None, the search will be performed in the base context.
+
+        Returns:
+            str or False: The name of the first global variable containing the given value, or False if not found.
+        """
+        if script_name is not None:
+            for each in self.global_vars[script_name].keys():
+                if self.global_vars[script_name][each] == value:
+                    return each
+        return False
+
+    def return_global_value(self, variable:str, script_name:str=None):
+        """
+        Returns the value of a specific global variable in a script.
+
+        Args:
+            variable (str): The name of the global variable to retrieve.
+            script_name (str, optional): The name of the script. If None, the global variable in the base context will be retrieved.
+
+        Returns:
+            any: The value of the specified global variable.
+        """
+        if script_name is not None and variable in self.global_vars[script_name]:
+            return self.global_vars[script_name][variable]
+
 class WindowManager:
     """
     A class to manage PySimpleGUI windows and their events.
