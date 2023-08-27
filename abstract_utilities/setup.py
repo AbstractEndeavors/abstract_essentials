7ab79956c77e4f211fd0dc5aa4fc244c70277368
putkoff <57512254+putkoff@users.noreply.github.com>
2023-08-26 23:13:25 -0500

    Add files via upload

---
+++ abstract_utilities/setup.py
@@ -0,0 +1,27 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='abstract_utilities',
+    version='0.1.1',
+    author='putkoff',
+    author_email='partners@abstractendeavors.com',
+    description='abstract_utilities is a collection of utility modules providing a variety of functions to aid in tasks such as data comparison, list manipulation, JSON handling, string manipulation, mathematical computations, and time operations.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AbstractEndeavors/abstract_essentials/tree/main/abstract_utilities',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=['setuptools>=66.1.1', 'pexpect>=4.8.0', 'abstract_security>=0.0.1'],
+   package_dir={"": "src"},
+   packages=setuptools.find_packages(where="src"),
+   python_requires=">=3.6",
+  
+
+)
