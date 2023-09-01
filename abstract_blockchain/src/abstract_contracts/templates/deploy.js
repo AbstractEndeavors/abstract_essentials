e711469774da28842939325d4db568eaa5a83a2e
putkoff <57512254+putkoff@users.noreply.github.com>
2023-09-01 05:30:35 -0500

    Create deploy.js

---
+++ abstract_blockchain/src/abstract_contracts/templates/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const Greeter = await hre.ethers.getContractFactory("math");
+  const greeter = await Greeter.deploy();
+  await greeter.deployed();
+  console.log("Greeter deployed to:", greeter.address);
+}
+function link(bytecode, libName, libAddress) {
+  let symbol = "__" + libName + "_".repeat(40 - libName.length - 2);
+  return bytecode.split(symbol).join(libAddress.toLowerCase().substr(2))
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
