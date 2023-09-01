e1429f2c0bb12bddf69c79d8bba82cc8d6ddc6b1
putkoff <57512254+putkoff@users.noreply.github.com>
2023-09-01 05:39:33 -0500

    Update setup.py

--- abstract_blockchain/setup.py
+++ abstract_blockchain/setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name='abstract_images',
-    version='0.0.0.5',
+    name='abstract_blockchain',
+    version='0.0.0.0',
     author='putkoff',
     author_email='partners@abstractendeavors.com',
-    description="This module, part of the `abstract_essentials` package, provides a collection of utility functions for working with images and PDFs, including loading and saving images, extracting text from images, capturing screenshots, processing PDFs, and more.",
+    description="i just wanna wear my clown suit on the outside and have my business suit on underneath. and if thats not how you feel, you're probably NGMI. feel free to contribute.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AbstractEndeavors/abstract_essentials/tree/main/abstract_images',
@@ -18,7 +18,7 @@ setuptools.setup(
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.11',
       ],
-    install_requires=['abstract_webtools>=0.1.0', 'pyscreenshot>=3.1', 'pdf2image>=1.16.3', 'pytesseract>=0.3.10', 'requests>=2.28.1', 'numpy>=1.25.2', 'abstract_utilities>=0.1.8', 'PyPDF2>=3.0.1', 'abstract_gui>=0.0.55.6'],
+    install_requires=[],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
